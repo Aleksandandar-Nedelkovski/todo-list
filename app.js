68331a18f056605b6213a90bdb8d5326affe28a8
Aleksandar Nedelkovski <aleks@nedelkovskialeks.com>
2019-10-14 03:24:54 -0500

    successfully delete item of the list

--- app.js
+++ app.js
@@ -53,14 +53,22 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", function (req, res) {
-    const item = req.body.newItem;
-    if (req.body.list === "Work List") {
-        workItems.push(item);
-        res.redirect("/work");
-    } else {
-        items.push(item)
-        res.redirect("/");
-    }
+    const itemName = req.body.newItem;
+    const item = new Item({
+        name: itemName
+    })
+    item.save();
+    res.redirect("/");
+});
+
+app.post("/delete", function (req, res) {
+    const checkedItemId = req.body.checkbox;
+    Item.findByIdAndRemove(checkedItemId, function (err) {
+        if (!err) {
+            console.log("Success");
+            res.redirect("/");
+        }
+    })
 });
 
 app.get("/work", function (req, res) {
