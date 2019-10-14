52b23bb50f6d8158464871cc901af011f1f2c3a2
Aleksandar Nedelkovski <aleks@nedelkovskialeks.com>
2019-10-13 23:44:07 -0500

    connected to mongoDB and  mongoose

--- app.js
+++ app.js
@@ -2,12 +2,13 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
 const date = require(__dirname + "/date.js")
 
 const app = express();
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+mongoose.connect('mongodb://localhost:27017/todolistDB', { useNewUrlParser: true });
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
